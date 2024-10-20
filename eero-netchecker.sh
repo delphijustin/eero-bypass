439084d911a38bcebedfa8b22cf6ae5dae64e9b3
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-20 09:25:05 -0500

    Update eero-netchecker.sh

--- eero-netchecker.sh
+++ eero-netchecker.sh
@@ -2,7 +2,7 @@
 export duration=3600
 # Function to check for internet connection
 check_internet() {
-    # Ping Google's DNS server (9.9.9.9) 1 time with a timeout of 3 seconds
+    # Ping DNS server (9.9.9.9) 1 time with a timeout of 3 seconds
     if ping -q -c 1 -W 3 9.9.9.9 >/dev/null; then
         return 0  # Internet is up
     else
