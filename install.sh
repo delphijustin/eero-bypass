91cbd9a382d0fe64cc92fa0599a5f5d1a9bffe19
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-20 17:15:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt install python3-selenium xvfb
 cp eero-bypass.py /usr/local/bin/
 cp eero-netchecker.sh /usr/local/bin/
 cp eero-onreconnect.sh /usr/local/bin/
-cp eero-bypass.service /etc/systemd/system/eero-bypass.service
+cat eero-bypass.service.default | sed "s/user_name/$2/g" > /etc/systemd/system/eero-bypass.service
 chmod +x /usr/local/bin/eero-netchecker.sh
 chmod +x /usr/local/bin/eero-onreconnect.sh
 systemctl enable eero-bypass.service
@@ -14,4 +14,4 @@ echo "To start the service type:"
 echo "sudo systemctl start eero-bypass.service"
 exit 0
 fi
-sudo $0 root
+sudo $0 root $USER
