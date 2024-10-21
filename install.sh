683c2ca8c1e194fa245ce09215d0c7aab52b04af
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-21 10:26:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,13 @@ cp eero-onreconnect.sh /usr/local/bin/
 cat eero-bypass.service.default | sed "s/user_name/$2/g" > /etc/systemd/system/eero-bypass.service
 chmod +x /usr/local/bin/eero-netchecker.sh
 chmod +x /usr/local/bin/eero-onreconnect.sh
+echo "Has not written to this file." > /root/wan-timer.txt
 systemctl enable eero-bypass.service
 echo ""
 echo "To start the service type:"
 echo "sudo systemctl start eero-bypass.service"
+echo ""
+echo "Log file is located in /root/wan-timer.txt"
 exit 0
 fi
 sudo $0 root $USER
