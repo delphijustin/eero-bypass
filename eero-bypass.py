4a158fd43ecf7d7b08cc86f6f1abd3a90d5b700c
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-23 16:07:57 -0500

    Update eero-bypass.py

--- eero-bypass.py
+++ eero-bypass.py
@@ -4,7 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import os
+import sys
+dnsip=sys.argv[1:][0]
 eeroexcept=0
 # Set up Chrome options to run in headless mode
 chrome_options = Options()
@@ -22,21 +23,113 @@ service = Service(chrome_driver_path)
 # Initialize the WebDriver
 driver = webdriver.Chrome(service=service)
 # Open the captive portal page
-try:
-	driver.get('http://'+os.getenv('dns_ip'))
+def resetConnection():
+	try:
+		driver.get("http://"+dnsip)
 # Find the button by its HTML attributes (e.g., ID, class, or name)
-	button = driver.find_element(By.CLASS_NAME, 'btn')
+		button = driver.find_element(By.CLASS_NAME, 'btn')
 
 # Simulate button click
-	button.click()
+		button.click()
 
 # Optional: wait for page load, interaction
-	time.sleep(5)  # Wait 15 seconds for demo purposes
-	button1 = driver.find_element(By.CLASS_NAME, 'btn')
-	button1.click()
+		time.sleep(5)  # Wait 5 seconds
+		button1 = driver.find_element(By.CLASS_NAME, 'btn')
+		button1.click()
+		return True
+	except:
+		return False
+while True:
+	resetConnection()
+	time.sleep(15)
+	driver.execute_script("location.href='http://"+dnsip+"';")  # JavaScript to retry
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import sys
+dnsip=sys.argv[1:][0]
+eeroexcept=0
+# Set up Chrome options to run in headless mode
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Enable headless mode
+chrome_options.add_argument("--no-sandbox")  # Required in some environments
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")  # Disable GPU hardware acceleration
 
-	time.sleep(7)
-except:
-	eeroexcept=1
-# Close browser
-driver.quit()
+# Define the path to your ChromeDriver
+chrome_driver_path = "/usr/bin/chromedriver"
+
+# Set up the Service object
+service = Service(chrome_driver_path)
+
+# Initialize the WebDriver
+driver = webdriver.Chrome(service=service)
+# Open the captive portal page
+def resetConnection():
+	try:
+		driver.get("http://"+dnsip)
+# Find the button by its HTML attributes (e.g., ID, class, or name)
+		button = driver.find_element(By.CLASS_NAME, 'btn')
+
+# Simulate button click
+		button.click()
+
+# Optional: wait for page load, interaction
+		time.sleep(5)  # Wait 5 seconds
+		button1 = driver.find_element(By.CLASS_NAME, 'btn')
+		button1.click()
+		return True
+	except:
+		return False
+while True:
+	resetConnection()
+	time.sleep(15)
+	driver.execute_script("location.href='http://"+dnsip+"';")  # JavaScript to retry
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import sys
+dnsip=sys.argv[1:][0]
+eeroexcept=0
+# Set up Chrome options to run in headless mode
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Enable headless mode
+chrome_options.add_argument("--no-sandbox")  # Required in some environments
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")  # Disable GPU hardware acceleration
+
+# Define the path to your ChromeDriver
+chrome_driver_path = "/usr/bin/chromedriver"
+
+# Set up the Service object
+service = Service(chrome_driver_path)
+
+# Initialize the WebDriver
+driver = webdriver.Chrome(service=service)
+# Open the captive portal page
+def resetConnection():
+	try:
+		driver.get("http://"+dnsip)
+# Find the button by its HTML attributes (e.g., ID, class, or name)
+		button = driver.find_element(By.CLASS_NAME, 'btn')
+
+# Simulate button click
+		button.click()
+
+# Optional: wait for page load, interaction
+		time.sleep(5)  # Wait 5 seconds
+		button1 = driver.find_element(By.CLASS_NAME, 'btn')
+		button1.click()
+		return True
+	except:
+		return False
+while True:
+	resetConnection()
+	time.sleep(15)
+	driver.execute_script("location.href='http://"+dnsip+"';")  # JavaScript to retry
