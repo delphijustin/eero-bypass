5b01f61b89044f9a7d6e9a42dd31eb81c3205cb7
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-23 16:09:04 -0500

    Update eero-bypass.sh

--- eero-bypass.sh
+++ eero-bypass.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-/usr/local/bin/eero-netchecker.sh display0
+echo "PLEASE IGNORE ANY BLOCKED MESSAGES EERO"
+echo "MAY TELL YOU. IT USUALLY MEANS YOU ARE "
+echo "ALREADY CONNECTED"
+echo ""
+echo "Also keep the chrome browser opened as"
+echo "it needs it to stay connected"
+source /etc/eero-bypass.conf
+nohup python3 /usr/local/bin/eero-bypass.py $dns_eero &
