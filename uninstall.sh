dfc561142d300e00b947f1f9561fd38f11cb02bc
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-23 16:13:53 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-systemctl stop eero-bypass.service
-systemctl disable eero-bypass.service
+rm eero-bypass.sh
 rm /usr/local/bin/eero-bypass.py
-rm /usr/local/bin/eero-netchecker.sh
 if [[ "$1" == "all" ]]
 then
-rm /usr/local/bin/eero-onreconnect.sh
 rm /etc/eero-bypass.conf
 rm ~/wan-timer.txt
 rm ~/eero-clicker.log
