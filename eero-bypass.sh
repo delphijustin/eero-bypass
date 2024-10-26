77059914324e0ed904b8aa0467f83113b4590b01
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-26 17:18:43 -0500

    Update eero-bypass.sh

--- eero-bypass.sh
+++ eero-bypass.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-echo "PLEASE IGNORE ANY BLOCKED MESSAGES EERO"
-echo "MAY TELL YOU. IT USUALLY MEANS YOU ARE "
-echo "ALREADY CONNECTED"
-echo ""
-echo "Also keep the chrome browser opened as"
-echo "it needs it to stay connected"
 source /etc/eero-bypass.conf
-nohup python3 /usr/local/bin/eero-bypass.py $dns_eero &
+if [[ "$1" == "stop" ]]
+then
+if [[ "$2" != "auto" ]]
+then
+echo "THIS WILL KILL ALL PYTHON,NCAT AND CHROME PROCESSES"
+echo "Continue?"
+read -p "Type 1 for yes, 0 for no: " choice
+else
+choice=1
+fi
+if [[ "$choice" == "0" ]]
+then
+exit 0
+fi
+if [[ "$choice" == "1" ]]
+then
+killall python3 chrome ncat
+fi
+exit 0
+fi
+touch /tmp/eero-status.http
+if [[ "$eero_status_port" != "0" ]]
+then
+nohup ncat -k -l $eero_status_port --sh-exec "/usr/local/bin/eero-httpd.sh" > eero-httpd.log 2>&1 &
+fi
+nohup python3 /usr/local/bin/eero-bypass.py $dns_eero $timelimit > eero-bypass.log 2>&1 &
