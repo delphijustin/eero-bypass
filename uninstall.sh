6c8f516748844801e4047692591da2c4ab19651d
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-10-26 17:33:12 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm eero-bypass.sh
 rm /usr/local/bin/eero-bypass.py
+rm /usr/local/bin/eero-httpd.sh
 if [[ "$1" == "all" ]]
 then
 rm /etc/eero-bypass.conf
-rm ~/wan-timer.txt
-rm ~/eero-clicker.log
+rm /tmp/eero-status.http
 fi
 rm ~/.config/autostart/eero-bypass.desktop
