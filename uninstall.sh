061dd09673ac4e94ce94a12c25abf4c0bc8e0aa9
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2025-04-03 09:30:51 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm eero-bypass.sh
+rm /usr/local/bin/eero-bypass.sh
 rm /usr/local/bin/eero-bypass.py
 rm /usr/local/bin/eero-httpd.sh
+rm /usr/share/eero-bypass/eerodefault.wav
+rmdir /usr/share/eero-bypass
 if [[ "$1" == "all" ]]
 then
 rm /etc/eero-bypass.conf
