120f33057316958059f660aa424d4f253c6cddf7
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2025-04-03 13:13:43 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm /usr/local/bin/eero-bypass.sh
 rm /usr/local/bin/eero-bypass.py
-rm /usr/local/bin/eero-httpd.sh
 rm /usr/share/eero-bypass/eerodefault.wav
+rm /usr/share/eero-bypass/eeropopup.sh
 rmdir /usr/share/eero-bypass
 if [[ "$1" == "all" ]]
 then
