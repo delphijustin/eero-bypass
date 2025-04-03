90f65154d151ba07037f85a2df57bf0c8fe7fdda
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2025-04-03 09:34:24 -0500

    Update eero-bypass.py

--- eero-bypass.py
+++ eero-bypass.py
@@ -4,35 +4,23 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
-import threading
+from datetime import timedelta
 import time
 import sys
+import random
 import os
+import socket
 import re
+lastOp = -1
+timeAlert = int(sys.argv[8])
+BROADCAST_IP = sys.argv[4]  # Broadcast address
+UDPPORT = int(sys.argv[5])  # Port to send to
+start_time = datetime.now()
+seconds_elapsed=0
+newError=-2
 oldError=-1
-dnsip=sys.argv[1:][0]
-eeroTime=int(sys.argv[1:][1])
-timerActive=False
-expires=-eeroTime
-resetAt=datetime.now()
-resetAtStr=resetAt.strftime("%H:%M:%S %m/%d/%Y");
-def eeroTimer():
-	timerActive = True
-	while timerActive:
-		expires = eeroTime
-		resetAt = datetime.now()
-		resetAtStr=resetAt.strftime("%H:%M:%S %m/%d/%Y");
-		for t in range(eeroTime):
-			expires=eeroTime-t
-			with open("/tmp/eero-status.http","w") as file:
-				file.write("<p>\nReset happend at "+resetAtStr+"<br>\n")
-				file.write(f"Timeleft: {expires} minute(s), currently on minute#{t}<br>\n")
-				file.write(f"Time limit configured: {eeroTime} minutes<br></p>\n")
-			time.sleep(60)
-
-if os.path.getsize("/tmp/eero-status.http")<1:
-	with open("/tmp/eero-status.http","w") as file:
-		file.write("No disconnects yet!")
+processIds = sys.argv[2]+"p"+str(os.getpid())+"p"+sys.argv[3]+"p"+sys.argv[6]
+eeroTime=int(sys.argv[1])
 # Set up Chrome options to run in headless mode
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Enable headless mode
@@ -48,10 +36,38 @@ service = Service(chrome_driver_path)
 
 # Initialize the WebDriver
 driver = webdriver.Chrome(service=service)
-# Open the captive portal page
+def udpSend(op):
+	global lastOp
+	if UDPPORT == 0:
+		return
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create UDP socket
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Enable broadcasting
+	if(op==0)and(op!=lastOp):
+		sock.sendto(b"EERO_HELLO\n", (BROADCAST_IP, UDPPORT))
+	if(op==1)and(op!=lastOp):
+		sock.sendto(b"EERO_DISCONNECT\n", (BROADCAST_IP, UDPPORT))
+	if(op==2)and(op!=lastOp):
+		sock.sendto(b"EERO_RECONNECT\n", (BROADCAST_IP, UDPPORT))
+	if(op==3)and(op!=lastOp):
+		eero_timercmd=f"EERO_TIMER{timeAlert}\n"
+		eero_timerbytes=eero_timercmd.encode("ascii")
+		sock.sendto(eero_timerbytes, (BROADCAST_IP, UDPPORT))
+	time.sleep(5)
+	lastOp=op
+	sock.close()
 def resetConnection():
 	try:
-		driver.get("http://"+dnsip)
+		htmlfailed = False
+		html = driver.page_source
+		if driver.title != "eero Timer":
+			html = '<html><head><title>eeroBypass - Reconnecting...</title><script>function redirectTimer(){setTimeout(function(){location.search="?lastUpdated="+new Date().toLocaleString().replaceAll(" ","");},10240);}</script></head><body onload="redirectTimer()">Reconnecting... Please wait</body></html>'
+		html_size = len(html)
+		with open("/tmp/eerostatus.http", "w") as file:
+			file.write(f"HTTP/1.0 200 OK\nServer: delphijustin eero-bypass\nContent-type: text/html\nContent-Length: {html_size}\nCache-Control: no-store, no-cache, must-revalidate, max-age=0\nPragma: no-cache\nExpires: 0\nX-Robots-Tag: noindex, nofollow\n\n{html}")
+	except:
+		htmlfailed=True
+	eeroTime=int(sys.argv[1])
+	try:
 # Find the button by its HTML attributes (e.g., ID, class, or name)
 		button = driver.find_element(By.CLASS_NAME, 'btn')
 # Simulate button click
@@ -61,23 +77,29 @@ def resetConnection():
 		time.sleep(5)  # Wait 5 seconds
 		button1 = driver.find_element(By.CLASS_NAME, 'btn')
 		button1.click()
+		udpSend(2)
 		time.sleep(7)
-		if not timerActive:
-			timerThr = threading.Thread(target=eeroTimer)
-			timerThr.start()
-			timerActive = True
 		return 0
 	except:
-		return 1
+		udpSend(1)
+	return 1
+udpSend(0)
 while True:
 	newError=resetConnection()
 	now = datetime.now()
 	if newError != oldError:
 		if newError == 0:
 			print(now.strftime("%H:%M:%S")+" Just got back online")
-		if newError == 1:
+			start_time = datetime.now()
+		elif newError == 1:
 			print(now.strftime("%H:%M:%S")+" You could be online or there was an error")
-	oldError=newError
-	driver.execute_script(f"document.body.innerHTML='Please keep this browser window running.<br>Last action was at {resetAtStr}<br>Last Error: {newError}<br><img src=\"//delphianserver.com/online-icon.php\"></p>';")
-	time.sleep(15)
-	driver.execute_script("location.href='http://"+dnsip+"';")  # JavaScript to retry
+	end_time = datetime.now()
+	olderror = newError
+	updatedAt = end_time.strftime("%m/%d/%Y,%I:%M:%S%p")
+	seconds_elapsed=int((end_time - start_time).total_seconds())
+	eeroTime=int(sys.argv[1])
+	if int(seconds_elapsed/60)==eeroTime-timeAlert:
+		udpSend(3)
+		time.sleep(55)
+	driver.execute_script(f"location.href='http://delphianserver.com/eerotest/?updatedAt={updatedAt}&time={seconds_elapsed}&limit={eeroTime}&pids={processIds}';")  # JavaScript to retry
+	time.sleep(int(sys.argv[7]))
